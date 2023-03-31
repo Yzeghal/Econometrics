92d880f49e3cf8390b5fd6086bb97c37c2115d5f
Yzeghal <yanis.zeghal@ensae.fr>
2023-03-31 21:09:10 +0200

    compared with built in function
    used wooldridge library for the Hc0 and Hc3 Heteroscedasticity tests
    and implemented them with lmtest library.
    Homemade estimators seem nice

--- Basic Regression.R
+++ Basic Regression.R
@@ -1,5 +1,9 @@
 library(matrixcalc) #for matrix inversion and others
 library(MASS)#to generate multivariate data
+library(lmtest) #to perform test with specified indications with coeftest
+library(sandwich) # to compute heteroscedasticity robust tests : vcovHC(reg, type = "HC0")
+#library(lmtest)
+#library(sandwich)
 generator<-function(dim1=2, dim2=2, range=100){
   #to generate random matrices and test functions
   l=dim1*dim2
@@ -8,11 +12,11 @@ generator<-function(dim1=2, dim2=2, range=100){
 }
   
 #generating data
-set.seed(0)
+#set.seed(0)
 eps=rnorm(1000, mean=0, sd=0.2)
 l=2
-d=diag(100)
-d2=cbind(d,matrix(rep(0,l)))[,2:(l+1)]
+d=diag(l)
+d2=cbind(d,matrix(rep(0,l)))[,l:(l+1)]
 d<-d+d2
 sigma<-(d+t(d))/2
 
@@ -20,8 +24,7 @@ beta= matrix(c(2,4))
 x=mvrnorm(1000,c(100,50),sigma[1:l,1:l])
 y=x%*%beta+eps
 
-#built-in model fitting
-reg=lm(y~x)
+
 
 coefs<-function(X,Y){
   nx=dim(X)[1]
@@ -36,7 +39,7 @@ coefs<-function(X,Y){
 estimate<-function(X,Beta_hat){
   nx=dim(X)[1]
   X=cbind(matrix(rep(1),nx),X)
-  Y_hat=X%*%Beta
+  Y_hat=X%*%Beta_hat
 }
 
 asymptotic_variance<-function(X,Y){
@@ -56,11 +59,26 @@ asymptotic_variance<-function(X,Y){
 }
 
 standard_errors<-function (var,nx){
-  s=sqrt(diag(var)/nx)
+  s=matrix(sqrt(diag(var)/nx))
 }
 
 student_t<-function(Beta_hat,var,nx,hyp = 0){
   sd=sqrt(diag(var)/nx)
   t=(Beta_hat-hyp)/sd
 }
-  
+#homemade model fitting
+Beta_hat = coefs(x,y)
+asvar=asymptotic_variance(x,y)
+se=standard_errors(asvar, 1000)
+
+#built-in model fitting
+reg=lm(y~x)
+reg=lm(y~x)
+tests=coeftest(reg, vcov = vcovHC(reg, type = "HC0")) #performs tests on the model reg with the variance matrix calculated with vcovHC
+
+#comparaison
+print(Beta_hat-tests[,1]) #difference between built-in and homemade estimators
+print(se-tests[,2]) #difference between built-in and homemade std error
+
+#difference are around 1e-10 with Hc0 and 1e-4 with Hc3 
+
